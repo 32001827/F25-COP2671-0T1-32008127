adff37d163718a88201327af84fd4008a46fbb68
32008127 <32008127@sfcollege.edu>
2025-09-25 22:47:49 -0400

    3.1 Complete

---
+++ Units/Player Control/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    private Rigidbody playerRB;
+    public float jumpForce = 10.0f;
+    public float gravityModifier;
+    public bool isOnGround = true;
+
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        // playerRB.AddForce(Vector3.up * 1000);
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+}
